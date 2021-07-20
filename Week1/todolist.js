4af3cfa8e13a3d89dd7b7f046a46b5ccfbda76f9
Warrick-B <69379931+Warrick-B@users.noreply.github.com>
2021-07-20 19:59:01 +1000

    Add files via upload

--- Week1/todolist.js
+++ Week1/todolist.js
@@ -1,15 +1,23 @@
-Vue.component('todo-item', {
-    props: ['todo'],
-    template: '<li>{{ todo.text }}</li>'
-})
-
-var app = new Vue({
-    el: '#app',
-    data: {
-      TodoList: [
-        { id: 0, text: 'Practical 1' },
-        { id: 1, text: 'Project Proposal' },
-        { id: 2, text: 'Project Implementation ' }
-      ]
-    }
-})
+const app = new Vue({
+el: '#app',
+data:{
+  title: 'ToDo List.',
+  newtodo:'',
+  todos:[],
+},
+methods: {
+  addTodo(){
+    this.todos.push({
+      title: this.newtodo,
+      done: false
+    });
+    this.newtodo = '' //clear out entry text box
+  },
+  
+  removeTodo(todo)
+  {
+    const todoIndex =this.todos.indexOf(todo);
+    this.todos.splice(todoIndex, 1);
+  }
+}
+});
