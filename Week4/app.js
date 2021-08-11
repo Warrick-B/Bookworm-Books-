430d7263b6b8bf60e985e137c5ae6e46914ef5c4
Warrick-B <69379931+Warrick-B@users.noreply.github.com>
2021-08-11 10:54:56 +1000

    Add files via upload

---
+++ Week4/app.js
@@ -0,0 +1,10 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+    message: 'Welcome to my first Vue page!',
+    name: "My name is Sam",
+    htmlrendering: "<h3>This is a heading 3</h3>",
+    htmlheading1:"<h1> This is a heading 1</h1>"
+
+  }
+})
---
+++ Week4/app2.js
@@ -0,0 +1,19 @@
+var app2 = new Vue({
+el: '#display',
+data:{
+visable:true,
+show: true
+},
+})
+
+var app4 = new Vue({
+  el: '#forloop',
+  data: {
+    units: [
+      { code: 'SIT120'},
+      { code: 'SIT113' },
+      { code: 'SIT111' },
+      { code: 'SIT102' }
+    ]
+  }
+})
