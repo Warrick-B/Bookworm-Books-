eb081e03f7b56ecae63cd897b18777f88ee45325
Warrick-B <69379931+Warrick-B@users.noreply.github.com>
2021-09-12 20:06:53 +1000

    Add files via upload

---
+++ Week7/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+<Product-Card> 
+  <template v-slot:image>
+    <img src="https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F6%2F2016%2F09%2Fhpsorcstone.jpg"> 
+     </template >
+  <template v-slot:title> <!-- naming the slot -->
+  Harry Potter and the Philosopher's Stone  <!--Title of the book -->
+  </template>
+   <template v-slot:price> 
+  $19.99 <!--Price of the book -->
+  </template>
+  <template v-slot:author> 
+   J.K Rowling <!--author of the book -->
+  </template>
+</Product-Card>
+</template>
+<!-- Reference: meredith(N/A), 'Harry potter harry potter and the philosopher's stone',  meredith, accessed 9 Septemeber 2021-->
+<script>
+import ProductCard from './components/ProductCard.vue'
+
+export default {
+  components:{
+    ProductCard,
+  },
+  
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
